3f717da608b86226851301fb26c317395d1eabcd
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-04 13:13:47 +0100

    add more linewidth

--- first.js
+++ first.js
@@ -1,26 +1,33 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 600, 600 ]
+  dimensions: [ 1080, 1080 ]
 };
-
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-    
-    const w = 60;  //px
-    const h = 60;
-    const gap = 20;   //distanza tra i quadrati
+    //aggiungo spessore alle linee (+1%)
+    context.lineWidth = width * 0.01;
+
+    const w   = width * 0.10;  //la larghezza ora sarà il 10% della larghezza del foglio, qualunque essa sia
+    const h   = height * 0.10;
+    const gap = width * 0.03;   //distanza tra i quadrati
+
+    const ix  = width * 0.17;  //setto anche i punti iniziali in percentuale
+    const iy  = height * 0.17;    
+
+    const inside = width * 0.05; //setto anche le misure delle figure interne
+
     let x;
     let y;
 
-    for  (let i = 0; i < 5 ; i ++) {
+    for (let i = 0; i < 5 ; i++) {
       //volendo aggiungere piu row di quadrati anche lungo l'asse y aggiungiamo loop annidato
-      for (let j=0; j < 5; j++)  {
-        x = 100 + (w + gap)*i;
-        y = 100 + (h + gap)*j; //aggiungiamo '+ (height + gap)*j' alla coordianata y perche le nuove righe si sviluppano appunto sull'asse y
+      for (let j = 0; j < 5 ; j++)  {
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j; //aggiungiamo '+ (height + gap)*j' alla coordianata y perche le nuove righe si sviluppano appunto sull'asse y
      
         context.beginPath();
         context.rect(x, y, w, h);
@@ -29,10 +36,11 @@ const sketch = () => {
         //nello stesso ciclo posso creare una nuova figura per esempio un cerchio all'interno dei quadrati
         // qui mostrerò i nuovi cerchi solo il 50% delle volte. Math.random restituisce un numero tra 0 e 1
         if (Math.random () > 0.5) {
+          
           context.beginPath();
-          context.arc (x + 30 , y + 30 , 20 ,0, Math.PI * 2);
+          context.arc (x + inside , y + inside , inside/2 , 0 , Math.PI * 2); 
           /* con '+ 30' stiamo ad indicare di quanti px il centro deve spostarsi sull'asse x e y 
-          per non intersecarsi con i quadrati, perchè voglio i cerchi all'interno dei rect. il terzo parametro è il raggio*/
+          per non intersecarsi con i quadrati, perchè voglio i cerchi all'interno dei rect. il terzo parametro è il raggio, il quinto la circonferenza*/
           context.stroke()   
           }
       }
