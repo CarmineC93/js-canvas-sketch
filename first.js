e11c5780db0935228fba7ae1aeaa693c2195801f
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-04 12:26:47 +0100

    create circles inside squares

--- first.js
+++ first.js
@@ -10,20 +10,28 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     
+    const w = 60;  //px
+    const h= 60;
+    const gap = 20;   //distanza tra i quadrati
+    let x = 100 + (w + gap)*i;
+    let y = 100 + (h + gap)*j; //aggiungiamo '+ (height + gap)*j' alla coordianata y perche le nuove righe si sviluppano appunto sull'asse y
+  
     for  (let i = 0; i < 5 ; i ++) {
       //volendo aggiungere piu row di quadrati anche lungo l'asse y aggiungiamo un nuovo loop dentro il loop
       for (let j=0; j<5; j++)  {
-        let width = 60;  //px
-        let height= 60;
-        let gap = 20;   //distanza tra i quadrati
-        let x = 100 + (width + gap)*i;
-        let y = 100 + (height + gap)*j; //aggiungiamo '+ (height + gap)*j' alla coordianata y perche le nuove righe si sviluppano appunto sull'asse y
-      
         context.beginPath();
         context.rect(x,y,width, height);
         context.stroke();
     
-
+        //nello stesso ciclo posso creare una nuova figura per esempio un cerchio all'interno dei quadrati
+        // qui mostrerò i nuovi cerchi solo il 50% delle volte. Math.random restituisce un numero tra 0 e 1
+        if (Math.random () > 0.5) {
+          context.beginPath();
+          context.arc (x + 30 , y + 30 , 20 ,0, Math.PI * 2);
+          /* con '+ 30' stiamo ad indicare di quanti px il centro deve spostarsi sull'asse x e y 
+          per non intersecarsi con i quadrati, perchè voglio i cerchi all'interno dei rect. il terzo parametro è il raggio*/
+          context.stroke()   
+          }
       }
     }
   };
